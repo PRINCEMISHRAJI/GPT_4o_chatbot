bf62410743a714424d6218298398dbb7e285e101
Ankit Mishra <50262987+PRINCEMISHRAJI@users.noreply.github.com>
2024-08-25 00:45:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,27 +6,27 @@ export default defineConfig({
   server: {
     proxy: {
       '/faq': {
-        target: 'http://localhost:3000', // Your backend server
+        target: 'https://gpt-4o-chatbot.onrender.com/', // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/faq/, '/faq'), // Forward /faq requests to /faq on the backend
       },
       '/lead': {
-        target: 'http://localhost:3000', // Your backend server
+        target: 'https://gpt-4o-chatbot.onrender.com/', // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/lead/, '/lead'), // Forward /lead requests to /lead on the backend
       },
       '/upload-image': {
-        target: 'http://localhost:3000', // Your backend server
+        target: 'https://gpt-4o-chatbot.onrender.com/', // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/upload-image/, '/upload-image'), // Forward /upload-image requests to /upload-image on the backend
       },
       '/welcome': {
-        target: 'http://localhost:3000', // Your backend server
+        target: 'https://gpt-4o-chatbot.onrender.com/', // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/welcome/, '/welcome'), // Forward /welcome requests to /welcome on the backend
       },
       '/service_options': {
-        target: 'http://localhost:3000', // Your backend server
+        target: 'https://gpt-4o-chatbot.onrender.com/', // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/service_options/, '/service_options'), // Forward /service_options requests to /service_options on the backend
       },
